3db1b7abc6ba77b53787e0f51b19be91273023eb
RyanODOWD <60102592+RyanODOWD@users.noreply.github.com>
2022-09-08 09:39:09 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,44 @@
+input.onButtonPressed(Button.A, function () {
+    if (input.lightLevel() < 60) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+        basic.pause(1000)
+        serial.writeLine("paper")
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+    basic.pause(1000)
+    serial.writeLine("rock")
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        # # . # #
+        # # . # #
+        `)
+    basic.pause(1000)
+    serial.writeLine("scissors")
+})
+serial.redirectToUSB()
